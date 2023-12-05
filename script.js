6bbc83d47a0826b11ab267cfe3d07e52686c38cd
Samyht777 <152994071+Samyht777@users.noreply.github.com>
2023-12-05 11:47:19 -0300

    cartaosamy

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+
+//pegar a tag img
+const img =  document.querySelector("#profile img")
+
+ if(html.classList.contains('light')){
+ img.setAttribute('src', './imagens/avatar-light.png')
+} else{
+    img.setAttribute('src', './imagens/avatar.png')
+}  
+
+//substituir a img
+
+}
